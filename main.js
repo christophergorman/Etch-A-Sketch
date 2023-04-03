b02127146c1362bef3c0daef36d88b4207698497
Christopher Gorman <cgorman990@gmail.com>
2023-04-03 15:44:04 -0700

    Finalize board functionality

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 let color = "black";
+let click = false;
+
 document.addEventListener('DOMContentLoaded', function() {
     createBoard(16);
+
+    document.querySelector('body').addEventListener('click', function(e) {
+        if(e.target.tagName != "BUTTON") {
+            click = !click;
+            let draw = document.querySelector('#draw');
+            if(click) {
+                draw.innerHTML = "You can draw";
+            }
+            else {
+                draw.innerHTML = "Click to draw";
+            }
+        }
+    })
     let btn_popup = document.querySelector('#popup');
     btn_popup.addEventListener('click', function() {
         let size = getSize();
@@ -38,11 +53,13 @@ function getSize() {
 }
 
 function colorDiv(){
-    if(color == 'random') {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else {
-        this.style.backgroundColor = "black"
+    if(click){
+        if(color == 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+            }
+        else {
+         this.style.backgroundColor = "black"
+        }
     }
 }
 
